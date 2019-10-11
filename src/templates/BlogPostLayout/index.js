960f11913c94f05dc7a0f7786c592436066703f9
zhangyu1818 <723156735@qq.com>
2019-10-11 21:43:40 +0800

    站点提交

--- src/templates/BlogPostLayout/index.js
+++ src/templates/BlogPostLayout/index.js
@@ -48,6 +48,24 @@ const Index = ({ data, pageContext }) => {
                     </NavigatorItem>
                 )}
             </FooterNavigator>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(){
+                    var bp = document.createElement('script');
+                    var curProtocol = window.location.protocol.split(':')[0];
+                    if (curProtocol === 'https') {
+                        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+                    }
+                    else {
+                        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+                    }
+                    var s = document.getElementsByTagName("script")[0];
+                    s.parentNode.insertBefore(bp, s);
+                    })();
+`,
+                }}
+            />
         </Layout>
     );
 };
