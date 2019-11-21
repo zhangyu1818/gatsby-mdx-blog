4eed10502341171187b220ac8457e723e5a422e0
zhangyu1818 <723156735@qq.com>
2019-11-21 22:27:24 +0800

    更新了依赖

--- src/Components/Bio/index.js
+++ src/Components/Bio/index.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import TypeIt from "typeit";
 import "./style.scss";
 
-const Index = () => (
-    <div className="bio-wrap">
-        <div className="avatar" />
-        <p className="description">
-            oh oh sometime ~
-            <br />
-            i get a good feeling~
-        </p>
-    </div>
-);
+const Index = () => {
+    const descriptionEle = useRef(null);
+    useEffect(() => {
+        new TypeIt(descriptionEle.current, {
+            strings: ["oh oh sometimes ~", "i get a good feeling ~"],
+            speed: 50,
+            waitUntilVisible: true,
+            cursor: false,
+        }).go();
+    }, []);
+    return (
+        <div className="bio-wrap">
+            <div className="avatar" />
+            <p className="description" ref={descriptionEle} />
+        </div>
+    );
+};
 
 export default Index;
