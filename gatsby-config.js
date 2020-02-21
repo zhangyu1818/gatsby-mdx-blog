ee4e225d05c1a232a6cbc124fb2c5dafee8c118d
zhangyu1818 <723156735@qq.com>
2020-02-21 11:28:33 +0800

    feat: 首页列表渲染

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/posts/`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -31,5 +38,8 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+    },
   ],
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,35 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import HomeLayout from "../Components/Layout/Home"
 import "./global.scss"
 import PostList, { PostItem } from "../Components/PostList"
 
-const IndexPage = () => <HomeLayout></HomeLayout>
+export const query = graphql`
+  query PostsQuery {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date
+          description
+          title
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => (
+  <HomeLayout>
+    <PostList>
+      {data.allMdx.nodes.map(
+        ({ frontmatter, id, fields, timeToRead, excerpt }) => (
+          <PostItem key={id} name={frontmatter.title} date={frontmatter.date} />
+        )
+      )}
+      }
+    </PostList>
+  </HomeLayout>
+)
 
 export default IndexPage
