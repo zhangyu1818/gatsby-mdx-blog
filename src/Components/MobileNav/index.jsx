947302e154b96a08bea5b5f5be3daa2b49cf6eed
zhangyu1818 <723156735@qq.com>
2020-02-26 14:44:22 +0800

    feat:mobile nav

--- src/Components/MobileNav/index.jsx
+++ src/Components/MobileNav/index.jsx
@@ -1,3 +1,50 @@
 import React from "react";
+import { motion, useCycle } from "framer-motion";
+import MenuToggle from "./MenuToggle";
 
-const MobieNav=()=>
+import "./style.scss";
+import Navigation from "./navigation";
+
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at calc(100vw - 74px) 30px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(1px at calc(100vw - 74px) 30px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
+const defaultConfig = [
+  { name: "Home", to: "/", direction: "right", top: "entry" },
+  { name: "Blog", to: "/blog", direction: "left" },
+  { name: "Projects", to: "/projects", direction: "left" },
+];
+
+const MobileNav = ({ config = defaultConfig }) => {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  return (
+    <motion.header
+      className="mobile-nav"
+      initial={false}
+      animate={isOpen ? "open" : "closed"}
+    >
+      <motion.div className="mobile-nav-container" variants={sidebar}>
+        <Navigation config={config} />
+      </motion.div>
+      <MenuToggle toggle={() => toggleOpen()} />
+    </motion.header>
+  );
+};
+
+export default MobileNav;
